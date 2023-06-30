c6c19ad0209df2283f2a5317072859b54eedcd35
RickardPettersson <rickardnp@gmail.com>
2023-06-30 20:46:35 +0200

    Added C# class for Package with the properties in the specification and a ReadOnly property named IsValid that check that the values is in the valid range

---
+++ Early_Bird_API/Early_Bird_API/Model/Package.cs
@@ -0,0 +1,33 @@
+﻿namespace Early_Bird_API.Model
+{
+    public class Package
+    {
+        public string KolliId { get; set; }
+        
+        /// <summary>
+        /// Saved in "gram"
+        /// </summary>
+        public uint Weight { get; set; }
+        
+        public uint Length { get; set; }
+        
+        public uint Height { get; set; }
+        
+        public uint Width { get; set; }
+        
+        public bool IsValid { get
+            {
+                // Check if package weight is over 20 kilogram, the value Weight is saved in gram
+                if (Weight > Misc.ValidationHelper.MaxWeight) return false;
+
+                if (Length > Misc.ValidationHelper.MaxLength) return false;
+                
+                if (Height > Misc.ValidationHelper.MaxHeight) return false;
+                
+                if (Width > Misc.ValidationHelper.MaxWidth) return false;
+                
+                return true;
+            }
+        }   
+    }
+}
