2091d07651b80ec821c9e212093211b4ba17881b
RickardPettersson <rickardnp@gmail.com>
2023-06-30 20:48:16 +0200

    A helper clas to hold the default package list of hard coded packages to use when no memory cache exists

---
+++ Early_Bird_API/Early_Bird_API/Misc/DefaultPackages.cs
@@ -0,0 +1,30 @@
+﻿using Early_Bird_API.Model;
+
+namespace Early_Bird_API.Misc
+{
+    public class DefaultPackages
+    {
+        // A default list of a valid package and a not valid package, if memory cache not found
+        public static List<Package> List()
+        {
+            return new List<Package>() { 
+                // Valid package
+                new Package() {
+                    KolliId = "999123456789123456",
+                    Weight = 1,
+                    Length = 2,
+                    Height = 3,
+                    Width = 4
+                },
+                // Not valid package
+                new Package() {
+                    KolliId = "999123456789987654",
+                    Weight = 99,
+                    Length = 27,
+                    Height = 54,
+                    Width = 74
+                }
+            };
+        }
+    }
+}
